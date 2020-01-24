c9d46fb38d450f8f10bb3683233921cd7da7d2d7
Vlad Panevnyk <vladyslav.panevnyk@symphony-solutions.eu>
2020-01-24 17:54:42 +0200

    create CreateQuestionPoolInteractor

---
+++ BusinessLogic/BusinessLogicTests/ReflectionModule/Interactors/QuestionPollInteractorTests.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionPollInteractorTests.swift
+//  BusinessLogic
+//
+//  Created by Vladyslav Panevnyk on 24.01.2020.
+//  Copyright © 2020 Vladyslav Panevnyk. All rights reserved.
+//
+
+@testable import BusinessLogic
+
+final class QuestionPollInteractorTests {
+}
